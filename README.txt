8234de709392ee0288a2a20d0006e34ee3e9127d
alzze <alzzeat@gmail.com>
2026-06-07 18:32:14 +0200

    add p2wpkh address derivation

--- README.txt
+++ README.txt
@@ -1,4 +1,6 @@
-just a raw Bitcoin transaction
-from scratch 
-on testnet
-only compressed pk 
+raw bitcoin transaction from scratch
+testnet · python
+tb1qh0ekc7hk9etjn6rylqrgpywa8z70qekyac24r6 -> tb1qfq3mzvlv8jcugtjcucvagtva6qwrexuxh78ucw
+
+compressed pk only
+p2wpkh inputs and outputs
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!.venv/bin/python3
 
-import random, hashlib, ecdsa
+import random, hashlib, ecdsa, bech32
 from base58 import b58encode, b58decode_check
 
 def base58check(payload:bytes) -> str:
     checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
     return b58encode(payload + checksum).decode("utf-8")
-
+  
 def private_key_generator():
   # stored in .private_key1 and .private_key2 files
     secp256k1_order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
@@ -36,14 +36,29 @@ def import_private_key(file:str) -> bytes:
   return private_key 
 
 
-def generate_public_key(priv_key: bytes) -> bytes:
+def generate_public_key(privkey: bytes) -> bytes:
   # K = k × G
-  k = int.from_bytes(priv_key, 'big')
+  k = int.from_bytes(privkey, 'big')
   K = ecdsa.SECP256k1.generator * k
   prefix = b'\x02' if K.y() % 2 == 0 else b'\x03'
 
   return prefix + K.x().to_bytes(32, 'big')
 
+def generate_p2wpkh(pubkey: bytes) -> str:
+  sha = hashlib.sha256(pubkey).digest()
+  h160   = hashlib.new("ripemd160", sha).digest()
+  hrp = 'tb'
+
+  prog   = bech32.convertbits(h160, 8, 5)       # bech32 alphabet, 32 = 2⁵
+  address   = bech32.bech32_encode(hrp, [0] + prog) 
+
+  return address
+
 if __name__ == "__main__":
   private_key1 = import_private_key(".private_key1")
   private_key2 = import_private_key(".private_key2")
+
+  address_1 = generate_p2wpkh(generate_public_key(private_key1))
+  address_2 = generate_p2wpkh(generate_public_key(private_key2))
+
+  print(address_1, address_2)
